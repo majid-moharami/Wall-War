f74a064ed9a76c1f9e58f7b4c62dbdc718e19325
Majid Moharami <68108673+majid-moharami@users.noreply.github.com>
2026-08-11 15:38:03 +0330

    Update Google Web Client ID
    Update the Google Web Client ID to match the current project configuration in the Google Cloud Console.

--- app/src/main/java/com/wallwar/data/auth/AuthConstants.kt
+++ app/src/main/java/com/wallwar/data/auth/AuthConstants.kt
@@ -2,5 +2,5 @@ package com.wallwar.data.auth
 
 object AuthConstants {
     // Paste your Web Client ID from Google Cloud Console here
-    const val GOOGLE_WEB_CLIENT_ID = "969192049743-gk5002esar2rc6qpg5mapo1spjm8018q.apps.googleusercontent.com"
+    const val GOOGLE_WEB_CLIENT_ID = "969192049743-0tmm7r7hrt6216kk0m0pfi7a4fqfkron.apps.googleusercontent.com"
 }
