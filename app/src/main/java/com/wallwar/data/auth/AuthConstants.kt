02961d945940abfacfd0a105e9767fce89d8fd3b
Majid Moharami <68108673+majid-moharami@users.noreply.github.com>
2026-08-11 13:44:20 +0330

    Update Google Web Client ID
    Set the production Google Web Client ID to enable Google Sign-In functionality.

--- app/src/main/java/com/wallwar/data/auth/AuthConstants.kt
+++ app/src/main/java/com/wallwar/data/auth/AuthConstants.kt
@@ -2,5 +2,5 @@ package com.wallwar.data.auth
 
 object AuthConstants {
     // Paste your Web Client ID from Google Cloud Console here
-    const val GOOGLE_WEB_CLIENT_ID = "YOUR_WEB_CLIENT_ID_HERE"
+    const val GOOGLE_WEB_CLIENT_ID = "969192049743-gk5002esar2rc6qpg5mapo1spjm8018q.apps.googleusercontent.com"
 }
